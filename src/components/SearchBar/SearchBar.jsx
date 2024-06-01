5f8105fd6f825021faac2ea763fd7df88c4443f4
a-nester <a_nester@ukr.net>
2024-06-01 23:06:00 +0300

    add-toast

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,10 +2,12 @@ import css from "./SearchBar.module.css";
 import { FcSearch } from "react-icons/fc";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { validSchema } from "../../helpers";
+import toast, { Toaster } from "react-hot-toast";
 
 export const SearchBar = ({ onSearch }) => {
   const handleSubmit = (initialValues, actions) => {
     actions.resetForm();
+    !initialValues.searchValue && toast("Need to add search word!");
     onSearch(initialValues.searchValue);
   };
 
@@ -26,7 +28,7 @@ export const SearchBar = ({ onSearch }) => {
             placeholder="Search images and photos"
           />
 
-          <button className={css.searchBtn}>
+          <button className={css.searchBtn} type="submit">
             <FcSearch className="svgSearchBtn" size="30" />
           </button>
           <div className={css.errorWrap}>
@@ -34,6 +36,19 @@ export const SearchBar = ({ onSearch }) => {
               className={css.error}
               name="searchValue"
               component="span"
+            />
+            <Toaster
+              toastOptions={{
+                className: "",
+                style: {
+                  border: "1px solid #713200",
+                  padding: "16px",
+                  color: "red",
+                },
+              }}
+              containerStyle={{
+                top: 110,
+              }}
             />
           </div>
         </Form>
